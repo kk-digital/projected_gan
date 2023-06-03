8ac5c62ba487c43c13c422f44b3798dc7b4087f2
lidotcircle <beam.analyman@gmail.com>
2023-06-04 00:15:36 +0800

    no metrics

--- training/traning_loop_i2i.py
+++ training/traning_loop_i2i.py
@@ -587,9 +587,11 @@ def training_loop(
             kid_val = best_kid + 1
             if rank == 0:
                 print('Evaluating metrics...')
-            results = eval_metrics(lambda img: G_ema_gen(img), eval_set=eval_set, cur_nimg=cur_nimg, run_dir=run_dir, device=device)
-            kid, fid_val = results[0]['kid'], results[0]['fid']
-            kid_val, _ = kid
+            # results = eval_metrics(lambda img: G_ema_gen(img), eval_set=eval_set, cur_nimg=cur_nimg, run_dir=run_dir, device=device)
+            # kid, fid_val = results[0]['kid'], results[0]['fid']
+            # kid_val, _ = kid
+            kid_val = 100
+            fid_val = 100
             if rank == 0:
                 training_stats.report('Metrics/FID', fid_val)
                 training_stats.report('Metrics/KID', kid_val)
